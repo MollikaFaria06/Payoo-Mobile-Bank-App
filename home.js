9db7a7270f0b95c80c0cc0d4e395f7b69fea4e42
Faria Mollika <149615629+MollikaFaria06@users.noreply.github.com>
2025-08-24 02:05:32 +0600

    added Add Money Section

---
+++ home.js
@@ -0,0 +1,31 @@
+const validPin = 1234;
+
+document.getElementById('add-money').addEventListener('click',function(e){
+    e.preventDefault();
+    console.log('add money btn clicked');
+    const bank = document.getElementById('bank').value ;
+    const accountNumber =  document.getElementById('account-number').value ;
+    const amount = parseInt(document.getElementById('add-amount').value);
+
+    const pin =  parseInt(document.getElementById('add-pin').value) ;
+
+    const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+
+    
+
+    if(accountNumber.length < 11){
+        alert("Please Provide Valid Account Number");
+        return;
+    }
+
+
+    if(pin !== validPin){
+        alert("Please Provide Valid Pin Number");
+        return;
+    }
+
+    const totalNewAvailableBalance = amount + availableBalance;
+
+    document.getElementById('available-balance').innerText = totalNewAvailableBalance;
+
+})
